671601b873e302c84c3a7d1e5dadec975252de5a
Igor Ranieri Elland <igor@elland.me>
2018-04-16 11:18:38 +0200

    Init.

---
+++ BiometricLocker/BiometricLocker.h
@@ -0,0 +1,19 @@
+//
+//  BiometricLocker.h
+//  BiometricLocker
+//
+//  Created by Igor Ranieri on 16.04.18.
+//  Copyright © 2018 Bakken&Bæck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BiometricLocker.
+FOUNDATION_EXPORT double BiometricLockerVersionNumber;
+
+//! Project version string for BiometricLocker.
+FOUNDATION_EXPORT const unsigned char BiometricLockerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BiometricLocker/PublicHeader.h>
+
+
---
+++ BiometricLocker/BiometricLocker.swift
@@ -0,0 +1,14 @@
+//
+//  BiometricLocker.swift
+//  BiometricLocker
+//
+//  Created by Igor Ranieri on 16.04.18.
+//  Copyright © 2018 Bakken&Bæck. All rights reserved.
+//
+
+import Foundation
+import LocalAuthentication
+
+open class BiometricLocker {
+
+}
